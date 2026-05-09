962bb919a6b2138619e057b00e8f1f4ae31d7cea
mesud_17 <mesud3818@gmail.com  git config --global user.email mesud3818@gmail.com  git config --global user.email mesud3818@gmail.com>
2026-05-09 10:18:19 +0300

    formating the docment

--- frontend/auth-app/src/pages/Profile/Profile.jsx
+++ frontend/auth-app/src/pages/Profile/Profile.jsx
@@ -3,71 +3,43 @@ import { fetchProfile } from "../../service/api";
 import "./Profile.css";
 
 function Profile() {
-
   const [user, setUser] = useState(null);
-
   useEffect(() => {
-
     const getProfile = async () => {
-
       try {
-
         const token = localStorage.getItem("token");
-
         const result = await fetchProfile(token);
-
         setUser(result.user);
-
       } catch (error) {
         console.log(error);
       }
     };
-
     getProfile();
-
   }, []);
-
   return (
     <div className="profile-container">
-
       <div className="profile-card">
-
-        <h1 className="profile-title">
-          Profile Page
-        </h1>
+        <h1 className="profile-title">Profile Page</h1>
 
         {user ? (
           <>
             <div className="profile-box">
-              <span className="profile-label">
-                User ID
-              </span>
-
+              <span className="profile-label">User ID</span>
               <p>{user.id}</p>
             </div>
-
             <div className="profile-box">
-              <span className="profile-label">
-                Email
-              </span>
-
+              <span className="profile-label">Email</span>
               <p>{user.email}</p>
             </div>
-
             <div className="profile-box">
-              <span className="profile-label">
-                Created At
-              </span>
-
+              <span className="profile-label">Created At</span>
               <p>{user.created_at}</p>
             </div>
           </>
         ) : (
           <p>Loading...</p>
         )}
-
       </div>
-
     </div>
   );
 }
